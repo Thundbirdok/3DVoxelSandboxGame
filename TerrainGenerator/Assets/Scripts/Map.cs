1bf949c6c7079888078be390cfbf93fb7f1a9967
ip613s07 <vyacheslavgorohov@mail.ru>
2020-01-23 21:53:15 +0700

    Add Canvas

---
+++ TerrainGenerator/Assets/Scripts/Map.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+
+    public RenderTexture MapTexture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Texture2D texRef = new Texture2D(MapTexture.width, MapTexture.height);        
+
+        for (int x = 0; x < MapTexture.width; ++x)
+        {
+
+            for (int y = 0; y < MapTexture.height; ++y)
+            {
+
+                texRef.SetPixel(x, y, Color.red);
+
+            }
+
+        }
+
+        texRef.Apply();
+
+        // Copy your texture ref to the render texture
+        Graphics.Blit(texRef, MapTexture);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
